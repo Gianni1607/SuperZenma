523be11a81785f439e461c17c46a9d3d9c7fc3df
Gianni Alberico <g.a01@hotmail.fr>
2021-12-26 15:09:46 +0100

    Fond infini + optimisation code + correction bugs (free pointeur musique / mario qui ne bouge plus quand on gagne)

--- main.c
+++ main.c
@@ -277,56 +277,9 @@ int main(int argc, char** argv){
 
         affichage(renderer, background, WINDOW_W, WINDOW_H, vide, poteauFin, chateau, mario, nombreGoombas, nombreBlocs, nombreItems, tableauGoombas, tableauBlocs, tableauItems);
 
-        /*
-        SDL_RenderCopy(renderer, background->texture, NULL, &(background->fond));
-        if(*background->x + *background->w < WINDOW_W){
-            *background->x += *background->w;
-            SDL_RenderCopy(renderer, background->texture, NULL, &(background->fond));
-            *background->x -= *background->w;
-
-            if(*background->x + *background->w < 0){
-                background->compteur++;
-                fprintf(stderr, "Compteur : %i\n", background->compteur);
-                *background->x = 0;
-            }
-        }
-        else if(*background->x > 0){
-            *background->x -= *background->w;
-            SDL_RenderCopy(renderer, background->texture, NULL, &(background->fond));
-            *background->x += *background->w;
-
-            if(*background->x > WINDOW_W){
-                background->compteur--;
-                fprintf(stderr, "Compteur : %i\n", background->compteur);
-                *background->x = WINDOW_W - *background->w;
-            }
-        }
-
-        SDL_RenderCopy(renderer, vide->texture, NULL, &(vide->fond));
-        SDL_RenderCopy(renderer, poteauFin->texturePoteau, NULL, &(poteauFin->poteau));
-        SDL_RenderCopy(renderer, poteauFin->textureDrapeau, NULL, &(poteauFin->drapeau));
-        SDL_RenderCopy(renderer, chateau->textureGauche, NULL, &(chateau->chateauGauche));
-
-        for(int i = 0; i < nombreGoombas; i++)
-            SDL_RenderCopy(renderer, tableauGoombas[i]->texture, NULL, &(tableauGoombas[i]->fond));
-
-        for(int i = 0; i < nombreBlocs; i++)
-            SDL_RenderCopy(renderer, tableauBlocs[i]->texture, NULL, &(tableauBlocs[i]->fond));
-
-        for(int i = 0; i < nombreItems; i++)
-            SDL_RenderCopy(renderer, tableauItems[i]->texture, NULL, &(tableauItems[i]->fond));
-
-        SDL_RenderCopy(renderer, mario->texture, NULL, &mario->rect);
-
-        SDL_RenderCopy(renderer, chateau->textureDroite, NULL, &(chateau->chateauDroit));
-
-        SDL_RenderPresent(renderer);
-        SDL_RenderClear(renderer);
-        */
+        //GESTION EVENEMENTS
 
         SDL_PollEvent(&event);
-
-        //GESTION EVENEMENTS
 
         switch(event.type)
         {
